518612e01ac6b9e24a8ba0223a0c985b3d6e92a4
nippermh <24083+nippermh@users.noreply.github.com>
2024-09-18 21:07:29 +1000

    donate btn and finishing nav

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu-btn");
 const closeMenuBtn = document.querySelector(".close-menu-btn");
-
+const overlay = document.querySelector(".overlay");
+//open and close the mobile menu
 function openMenu() {
     menu.classList.add("open");
     menu.style.transition = "transform 0.5s ease";
@@ -15,7 +16,7 @@ function closeMenu() {
     menu.style.transition = "transform 0.5s ease";
     document.removeEventListener('click', closeMenuOutside);
 }
-
+//clicking outside the menu closes it
 function closeMenuOutside(event) {
     if (!menu.contains(event.target) && !openMenuBtn.contains(event.target)) {
         closeMenu();
